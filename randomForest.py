f211d6bab2da77589b38220e6e5a205b2afa9132
VirdiRajvir <rajvirsinghvirdi1@gmail.com>
2024-10-03 11:46:05 +0800

    Added csvs, RFM works, low accuracy

--- randomForest.py
+++ randomForest.py
@@ -1,11 +1,21 @@
 import pandas as pd
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
 
-data = pd.read_csv("fraudDetection_Datathon/csv files/undersampled_dataset_with_numerics.csv")
+data = pd.read_csv("fraudDetection_Datathon/csv files/undersampled_numeric_timeconvert.csv")
 y = data['is_fraud']
 X = data.drop('is_fraud', axis=1)
 
-print(f'data shape: {data.shape}')
-print(f'X shape: {X.shape}')
+data_test = pd.read_csv("fraudDetection_Datathon/csv files/test_undersampled_numerics.csv")
+y_test = data_test['is_fraud']
+X_test = data_test.drop('is_fraud', axis=1)
 
-print(data.head())
-print(data.isnull().sum())
+rf_Model = RandomForestClassifier()
+rf_Model.fit(X, y)
+
+
+print(X.shape)
+print(f'Train Accuracy: {rf_Model.score(X, y):.3f}')
+print(f'Test Accuracy: {rf_Model.score(X_test, y_test):.3f}')
+
+# print(data.head())
